050ae853fe628bb7990067296b878dffdd1fa553
Carlos Demetrio Alves Ribeiro <carlos.demetrio@castgroup.com.br>
2022-10-14 20:06:03 -0300

    Initial commit

---
+++ 1-java-basics/1 - escopo-variavel/exercicio4.java
@@ -0,0 +1,10 @@
+
+
+// da erro de compilacao
+public class Exercicio4 {
+	static int x = 15;
+    public static void main(String[] x) {
+       x = 200; // int cannot convert to string
+	   System.out.println(x);
+	}
+}
---
+++ 1-java-basics/1 - escopo-variavel/exercicio5.java
@@ -0,0 +1,11 @@
+
+
+// da erro de compilacao
+public class Exercicio5 {
+	static int i = 3;
+    public static void main(String[] a) { // compila e imprime os numeros de 10 ate 99
+      for(new Exercicio5().i = 10; new Exercicio5().i < 100; new Exercicio5().i++){
+		System.out.println(i);
+	  }
+	}
+}
