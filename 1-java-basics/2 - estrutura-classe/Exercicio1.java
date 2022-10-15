9734c20f14270838ec40e3caf85aff86dbb6d007
Carlos Demétrio <demetrio7500@gmail.com>
2022-10-15 19:37:36 -0300

    modulo 1.2

---
+++ 1-java-basics/2 - estrutura-classe/Exercicio1.java
@@ -0,0 +1,11 @@
+class Exercicio1 {
+    int Exercicio1 = 305;
+    // COMPILA E NAO IMPRIME NADA
+    void Exercicio1(){
+        System.out.println(Exercicio1);
+
+    }
+    public static void main(String[] args) {
+        new Exercicio1();
+    }
+}
